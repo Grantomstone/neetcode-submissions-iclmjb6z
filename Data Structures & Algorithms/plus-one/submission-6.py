4c45a72883b0b19fdc6b9d7a55ea313d9c2c5222
Grantomstone <66666918+Grantomstone@users.noreply.github.com>
2026-06-07 15:16:13 +0530

    Add: plus-one - submission-6

---
+++ Data Structures & Algorithms/plus-one/submission-6.py
@@ -0,0 +1,17 @@
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        digits.reverse()
+        carry = 0
+        for i, v in enumerate(digits):
+            if v != 9:
+                digits[i] += 1
+                digits.reverse()
+                return digits
+            else:
+                digits[i] = 0
+                carry = 1
+
+        if carry == 1:
+            digits.append(1)
+        digits.reverse()
+        return digits
